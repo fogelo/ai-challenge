e80bfbda11a4a380e494b7de6355e8747022d86d
anton <fogelo@yandex.ru>
2026-02-28 18:38:14 +0300

    feat(strategies): add index export file

---
+++ day10/src/strategies/index.ts
@@ -0,0 +1,4 @@
+export { IContextStrategy } from './IContextStrategy.js';
+export { SlidingWindowStrategy } from './SlidingWindowStrategy.js';
+export { StickyFactsStrategy } from './StickyFactsStrategy.js';
+export { BranchingStrategy } from './BranchingStrategy.js';
